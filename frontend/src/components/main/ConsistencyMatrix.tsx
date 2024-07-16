7c15f18ef58a27f73f1d9760155eeac2d6334c62
Weiberle17 <maxweiberle63@gmail.com>
2024-07-16 10:55:13 +0200

    fix: Remove hardcoded matrix size
    Still errors if there are more than 2 FutureProjections for a given KeyFactor.

--- frontend/src/components/main/ConsistencyMatrix.tsx
+++ frontend/src/components/main/ConsistencyMatrix.tsx
@@ -32,9 +32,14 @@ const ConsistencyMatrix: React.FC = () => {
     if (scenarioProject_id) {
       getFutureProjections(scenarioProject_id);
       getKeyFactors(scenarioProject_id);
-      initiateMatrix();
     }
   }, [scenarioProject_id]);
+
+  React.useEffect(() => {
+    if (futureProjections) {
+      initiateMatrix();
+    }
+  }, [futureProjections]);
 
   const getFutureProjections = async (scenarioProject_id: number) => {
     await axios.get("http://localhost:3001/db/fp/sp/" + scenarioProject_id)
@@ -52,11 +57,8 @@ const ConsistencyMatrix: React.FC = () => {
       .catch(error => console.error(error))
   };
 
-  console.log(futureProjections);
-  console.log(keyFactors);
   const initiateMatrix = () => {
-    console.log(futureProjections.length);
-    const size = 6;
+    const size = futureProjections.length;
     setMatrix(Array.from({ length: size }, () => Array(size).fill(0)));
   };
 
@@ -74,171 +76,173 @@ const ConsistencyMatrix: React.FC = () => {
 
   const acceptedValues = [1, 2, 6, 8, 9];
 
-  return (
-    <Box sx={{ width: "100%", margin: "0 auto", mt: 4 }}>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Konsistenzmatrix
-      </Typography>
-      <Box sx={{ display: "flex", justifyContent: "left", my: 2 }}>
-        <Button variant="contained" className='bg-primary hover:bg-primary-hover mr-4' type="submit">
-          Konsitenzwerte speichern
-        </Button>
-        <Button variant="outlined" color="secondary" >
-          Cancel
-        </Button>
-      </Box>
-      <TableContainer
-        component={Paper}
-        sx={{
-          maxHeight: "80vh", // Set a max height for the table container
-          overflowY: "auto", // Enable vertical scrolling
-        }}
-      >
-        <Table stickyHeader>
-          <TableHead>
-            <TableRow sx={{ zIndex: 4 }}>
-              <TableCell />
-              <TableCell />
-              {keyFactors.map((kf, kfIndex) => (
-                <TableCell
-                  key={`kf-head-${kfIndex}`}
-                  colSpan={2}
-                  align="center"
-                  sx={{
-                    position: "sticky",
-                    top: 0,
-                    zIndex: 4,
-                    background: "#fff",
-                    maxWidth: "200px", // Set a maximum width for wrapping
-                    whiteSpace: "normal", // Allow wrapping
-                    wordBreak: "break-word", // Break words if necessary
-                    overflowWrap: "break-word", // Handle overflow
-                  }}
-                >
-                  {kf.name}
-                </TableCell>
-              ))}
-            </TableRow>
-            <TableRow >
-              <TableCell sx={{ zIndex: 4 }} />
-              <TableCell sx={{ zIndex: 4 }} />
-              {futureProjections.map((proj, index) => (
-                <TableCell
-                  key={`proj-head-${index}`}
-                  align="center"
-                  sx={{
-                    position: "sticky",
-                    top: 0,
-                    zIndex: 5,
-                    background: "#fff",
-                    maxWidth: "200px", // Set a maximum width for wrapping
-                    whiteSpace: "normal", // Allow wrapping
-                    overflowWrap: "break-word", // Handle overflow
-                  }}
-                >
-                  {proj?.name ?? ""}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {futureProjections.map((projRow, i) => (
-              <TableRow key={`row-${i}`} sx={{ height: 70, background: "#fff", zIndex: 4 }}>
-                {i % 2 === 0 && (
+  if (matrix.length > 0) {
+    return (
+      <Box sx={{ width: "100%", margin: "0 auto", mt: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Konsistenzmatrix
+        </Typography>
+        <Box sx={{ display: "flex", justifyContent: "left", my: 2 }}>
+          <Button variant="contained" className='bg-primary hover:bg-primary-hover mr-4' type="submit">
+            Konsitenzwerte speichern
+          </Button>
+          <Button variant="outlined" color="secondary" >
+            Cancel
+          </Button>
+        </Box>
+        <TableContainer
+          component={Paper}
+          sx={{
+            maxHeight: "80vh", // Set a max height for the table container
+            overflowY: "auto", // Enable vertical scrolling
+          }}
+        >
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow sx={{ zIndex: 4 }}>
+                <TableCell />
+                <TableCell />
+                {keyFactors.map((kf, kfIndex) => (
                   <TableCell
-                    rowSpan={2}
+                    key={`kf-head-${kfIndex}`}
+                    colSpan={2}
+                    align="center"
+                    sx={{
+                      position: "sticky",
+                      top: 0,
+                      zIndex: 4,
+                      background: "#fff",
+                      maxWidth: "200px", // Set a maximum width for wrapping
+                      whiteSpace: "normal", // Allow wrapping
+                      wordBreak: "break-word", // Break words if necessary
+                      overflowWrap: "break-word", // Handle overflow
+                    }}
+                  >
+                    {kf.name}
+                  </TableCell>
+                ))}
+              </TableRow>
+              <TableRow >
+                <TableCell sx={{ zIndex: 4 }} />
+                <TableCell sx={{ zIndex: 4 }} />
+                {futureProjections.map((proj, index) => (
+                  <TableCell
+                    key={`proj-head-${index}`}
+                    align="center"
+                    sx={{
+                      position: "sticky",
+                      top: 0,
+                      zIndex: 5,
+                      background: "#fff",
+                      maxWidth: "200px", // Set a maximum width for wrapping
+                      whiteSpace: "normal", // Allow wrapping
+                      overflowWrap: "break-word", // Handle overflow
+                    }}
+                  >
+                    {proj?.name ?? ""}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {futureProjections.map((projRow, i) => (
+                <TableRow key={`row-${i}`} sx={{ height: 70, background: "#fff", zIndex: 4 }}>
+                  {i % 2 === 0 && (
+                    <TableCell
+                      rowSpan={2}
+                      component="th"
+                      scope="row"
+                      sx={{
+                        position: "sticky",
+                        left: 0,
+                        zIndex: 1,
+                        background: "#fff",
+                        width: "200px", // Set a maximum width for wrapping
+                        whiteSpace: "normal", // Allow wrapping
+                        overflowWrap: "break-word", // Handle overflow
+                      }}
+                    >
+                      {keyFactors[Math.floor(i / 2)].name}
+                    </TableCell>
+                  )}
+                  <TableCell
                     component="th"
                     scope="row"
                     sx={{
                       position: "sticky",
                       left: 0,
-                      zIndex: 1,
+                      zIndex: 2,
                       background: "#fff",
-                      width: "200px", // Set a maximum width for wrapping
+                      maxWidth: "250px", // Set a maximum width for wrapping
                       whiteSpace: "normal", // Allow wrapping
                       overflowWrap: "break-word", // Handle overflow
+                      lineBreak: "normal",
                     }}
                   >
-                    {keyFactors[Math.floor(i / 2)].name}
+                    {projRow?.name ?? ""}
                   </TableCell>
-                )}
-                <TableCell
-                  component="th"
-                  scope="row"
-                  sx={{
-                    position: "sticky",
-                    left: 0,
-                    zIndex: 2,
-                    background: "#fff",
-                    maxWidth: "250px", // Set a maximum width for wrapping
-                    whiteSpace: "normal", // Allow wrapping
-                    overflowWrap: "break-word", // Handle overflow
-                    lineBreak: "normal",
-                  }}
-                >
-                  {projRow?.name ?? ""}
-                </TableCell>
-                {futureProjections.map((projCol, j) => (
-                  <TableCell
-                    key={`cell-${i}-${j}`}
-                    align="center"
-                    sx={{
-                      background: '#fff',
-                      bgcolor:
-                        i >= j || j >= -i
-                          ? isSameKeyFactor(i, j)
-                            ? "grey.500"
-                            : "gray.400"
-                          : "white",
-                      zIndex: 1,
-                      position: "sticky",
-                    }}
-                  >
-                    {i > j ? (
-                      isSameKeyFactor(i, j) ? (
+                  {futureProjections.map((projCol, j) => (
+                    <TableCell
+                      key={`cell-${i}-${j}`}
+                      align="center"
+                      sx={{
+                        background: '#fff',
+                        bgcolor:
+                          i >= j || j >= -i
+                            ? isSameKeyFactor(i, j)
+                              ? "grey.500"
+                              : "gray.400"
+                            : "white",
+                        zIndex: 1,
+                        position: "sticky",
+                      }}
+                    >
+                      {i > j ? (
+                        isSameKeyFactor(i, j) ? (
+                          <Box
+                            sx={{
+                              bgcolor: "white",
+                              width: "100%",
+                              height: "100%",
+                              zIndex: 2,
+                            }}
+                          />
+                        ) : (
+                          <Select
+                            value={matrix[i][j]}
+                            onChange={(e) =>
+                              handleChange(i, j, Number(e.target.value))
+                            }
+                            displayEmpty
+                            sx={{ width: "60px", zIndex: 2, background: "#fff" }}
+                          >
+                            {acceptedValues.map((value) => (
+                              <MenuItem key={value} value={value}>
+                                {value}
+                              </MenuItem>
+                            ))}
+                          </Select>
+                        )
+                      ) : (
                         <Box
                           sx={{
-                            bgcolor: "white",
+                            bgcolor: "grey.300",
                             width: "100%",
                             height: "100%",
                             zIndex: 2,
                           }}
                         />
-                      ) : (
-                        <Select
-                          value={matrix[i][j]}
-                          onChange={(e) =>
-                            handleChange(i, j, Number(e.target.value))
-                          }
-                          displayEmpty
-                          sx={{ width: "60px", zIndex: 2, background: "#fff" }}
-                        >
-                          {acceptedValues.map((value) => (
-                            <MenuItem key={value} value={value}>
-                              {value}
-                            </MenuItem>
-                          ))}
-                        </Select>
-                      )
-                    ) : (
-                      <Box
-                        sx={{
-                          bgcolor: "grey.300",
-                          width: "100%",
-                          height: "100%",
-                          zIndex: 2,
-                        }}
-                      />
-                    )}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Box>
-  );
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    );
+  }
 };
 
 export default ConsistencyMatrix;
