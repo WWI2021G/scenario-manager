0da534b048da39644f84f8b05484ea887c995692
Weiberle17 <maxweiberle63@gmail.com>
2024-07-20 15:50:17 +0200

    feat: Automatically setup database on first server start

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import scenarioRoutes from "./routes/scenarioRoutes";
 import cors from "cors";
 import dbRoutes from "./routes/dbRoutes";
+import { appendFileSync } from "fs";
+import { dbService } from "./services/dbService";
 
 dotenv.config();
 
@@ -14,6 +16,16 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/scenario", scenarioRoutes);
 app.use("/db", dbRoutes);
+
+if (!process.env.DBSETUP) {
+  try {
+    appendFileSync("./.env", "DBSETUP=true");
+    dbService.setupDB();
+    console.log("Database setup completed");
+  } catch (error) {
+    console.error("Error setting variable to setup database");
+  }
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
